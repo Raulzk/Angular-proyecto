3db2382ac4843d9f4ad095e3f3e419d2f809f5a0
Raulzk <u20201c316@upc.edu.pe>
2023-04-12 11:31:00 -0500

    agregue navbar

---
+++ src/app/models/user.ts
@@ -0,0 +1,15 @@
+export class User {
+    constructor(
+        public id: number,
+        public name: string,
+        public surname: string,
+        public role: string,
+        public email: string,
+        public password: string,
+        public descripcion: string,
+        public image: string,
+
+
+
+    ) { }
+}
